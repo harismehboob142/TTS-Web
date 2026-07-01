1c3884b4e7086a271663bbdbdcdb96b544bd53f9
harismehboob142 <harismehboob2019@gmail.com>
2026-07-01 14:53:34 +0500

    test4

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -11,7 +11,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
+// 
 export const metadata: Metadata = {
   title: "Pussy Voice to Text Model",
   description: "Text-to-speech using Kitten TTS",
